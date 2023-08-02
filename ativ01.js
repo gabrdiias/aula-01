2956ac085786a3b2a7fa5e74aa3a42ad4a8a907b
gabrdiias <127976037+gabrdiias@users.noreply.github.com>
2023-08-02 10:53:12 -0300

    Add files via upload

---
+++ ativ01.js
@@ -0,0 +1,94 @@
+// Questão 01
+
+var favoriteFood = 'pizza'
+
+// Questão 02
+
+var numOfSlices = '8'
+
+// Questão 03
+
+console.log (favoriteFood)
+
+// Questão 04
+
+console.log (numOfSlices)
+
+// Questão 05
+//
+
+// Questão 06
+
+let changeMe = (true)
+
+// Questão 06
+
+changeMe = (false)
+
+// Questão 07
+
+console.log (changeMe)
+
+// Questão 08
+// código rodou com sucesso
+
+// Questão 09
+
+const entree = 'Enchiladas'
+
+// Questão 10
+
+console.log (entree)
+
+// Questão 11
+
+//Nesse exercício houve um erro gerado ao tentar reatribuir um valor a variável 'const', houve um erro.
+
+// Questão 12
+
+let levelUp = 10
+levelUp += 5
+console.log (levelUp)
+
+// Questão 13
+
+let powerLevel = 10
+levelUp -= 100
+console.log (powerLevel)
+
+// Questão 14
+
+let multiplyMe = 10
+levelUp *= 11
+console.log (multiplyMe)
+
+// Questão 15
+
+let quarterMe = 10
+levelUp /= 4
+console.log (quarterMe)
+
+// Questão 16
+
+let gainedDollar = 10;
+levelUp ++;
+console.log (gainedDollar)
+
+// Questão 17
+
+let lostDolar = 10;
+levelUp --;
+console.log (lostDolar)
+
+// Questão 18
+
+let myName = 'Gabriel';
+let myCity = 'Osasco';
+console.log (`My name is ${myName}. My favorite city is ${myCity}.`)
+
+// Questão 19
+
+let newVariable = 40;
+console.log (typeof newVariable);
+
+// Terminei ;-;
